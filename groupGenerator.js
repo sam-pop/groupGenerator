dca87023359d84c54044fe594600f1b7aa7e69d9
sam-pop <poplovitch.samuel@gmail.com>
2018-09-14 16:09:00 -0400

    fixed comments

--- groupGenerator.js
+++ groupGenerator.js
@@ -14,29 +14,29 @@ const groupSize = data.groupSize;
 const flags = ["A"];
 // -----------------------------------
 
-// runs until no names left ungrouped
+// Runs until all names are grouped
 while (names.length > 0) {
-    const tempArr = [];
-    for (let i = 0; i < groupSize; i++) {
+  const tempArr = [];
+  for (let i = 0; i < groupSize; i++) {
     // checks that we have names left in the name-pool
-        if (names.length > 0) {
-            shuffle(names);
-            tempArr.push(names.pop());
-            // if the names do not divide equally into groups -> adds the extra person to the last created group
-            if (!isDividedEvenly() && names.length == 1) {
-                tempArr.push(names.pop());
-            }
-        }
+    if (names.length > 0) {
+      shuffle(names);
+      tempArr.push(names.pop());
+      // if the names do not divide equally into groups --> adds the extra person to the last created group
+      if (!isDividedEvenly() && names.length == 1) {
+        tempArr.push(names.pop());
+      }
     }
-    // if a person is marked as a flag => switch places in the array
-    if (groupSize == 2) {
-        for (f of flags) {
-            if (tempArr[0].indexOf(f) > -1) {
-                switchPlaces(tempArr);
-            }
-        }
+  }
+  // if a person is marked as a flag --> switch places in the array
+  if (groupSize == 2) {
+    for (f of flags) {
+      if (tempArr[0].indexOf(f) > -1) {
+        switchPlaces(tempArr);
+      }
     }
-    console.log(tempArr);
+  }
+  console.log(tempArr);
 }
 
 /*
@@ -46,37 +46,37 @@ while (names.length > 0) {
 
 // Randomly shuffle the array
 function shuffle(array) {
-    var currentIndex = array.length,
-        randomIndex,
-        temporaryValue;
+  var currentIndex = array.length,
+    randomIndex,
+    temporaryValue;
 
-    // While there remain elements to shuffle...
-    while (currentIndex !== 0) {
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
     // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 function switchPlaces(arr) {
-    const temp = arr[0];
-    arr[0] = arr[1];
-    arr[1] = temp;
+  const temp = arr[0];
+  arr[0] = arr[1];
+  arr[1] = temp;
 }
 
 function isDividedEvenly() {
-    if (names.length % groupSize == 0) {
-        return true;
-    }
+  if (names.length % groupSize == 0) {
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 // -----------------------------------
